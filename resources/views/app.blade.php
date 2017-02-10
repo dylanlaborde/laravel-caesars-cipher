b91d80e5aa5b186a43bac7bd465d319433c9e2ba
Dylan <dylan4047@gmail.com>
2017-02-10 11:00:11 +0100

    ajout vue de templating

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.7/semantic.min.css">
+	<meta charset="UTF-8">
+</head>
+<body>
+	
+		@yield('content')
+	
+
+
+</body>
+</html>
